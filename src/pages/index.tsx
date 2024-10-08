1b5c53f7ad9a57b4f7aa1d364e1ba36ced46725d
蔡靖云 <caijingyun@cnlemon.net>
2024-10-08 10:48:27 +0800

    随意写个页面变量导出可以了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import store from '@/store';
+import store from "@/store";
 
 export default function IndexPage() {
-  const [userState, userDispatchers] = store.useModel('user');
+  const [userState, userDispatchers] = store.useModel("user");
 
   return (
-    <div >
+    <div>
       <header>
-        <p>
-          {JSON.stringify(userState)}
-        </p>
+        <p>{JSON.stringify(userState)}</p>
       </header>
-
     </div>
   );
 }
+
+// 随意写个暴露的变量就可以了，注释掉这行代码就会报Cannot read properties of null (reading 'store')
+// export const a = [];
